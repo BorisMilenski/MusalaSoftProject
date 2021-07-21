70dadf75a36374e66e2742d1adad34946a679bbb
BorisMilenski <boris.milenski@abv.bg>
2021-07-21 14:20:41 +0300

    Pushing exceptions up a level for management in ClientHandler. Checking whether the provided ID for editTask() and removeTask() exists in the database, if not, throws IllegalArgumentException.

--- src/main/java/entities/DAO.java
+++ src/main/java/entities/DAO.java
@@ -1,10 +1,11 @@
 package entities;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface DAO { //TODO: Refactor after merge, DAO is an independent pattern
-    List<Task> getTasks();
-    void addTask(Task task);
-    void editTask(int id, Task task);
-    void removeTask(int id, Task task);
+    List<Task> getTasks() throws SQLException;
+    void addTask(Task task) throws SQLException;
+    void editTask(int id, Task task) throws SQLException, IllegalArgumentException;
+    void removeTask(int id) throws SQLException, IllegalArgumentException;
 }
