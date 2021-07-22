6ac965deccd9385fc19e98f60daf8c0ce260c4c0
Nikkson <nicky.iliev@abv.bg>
2021-07-22 15:51:56 +0300

    Moved main menu logic to Menu

--- src/main/java/clientserver/ClientHandler.java
+++ src/main/java/clientserver/ClientHandler.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
-import java.time.LocalDateTime;
 import java.util.HashMap;
 
 public class ClientHandler extends Thread {
@@ -24,8 +23,6 @@ public class ClientHandler extends Thread {
 
     @Override
     public void run() {
-        String input;
-
         try {
             BufferedReader userInputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintStream messageToClient = new PrintStream(socket.getOutputStream(),true);
@@ -46,65 +43,15 @@ public class ClientHandler extends Thread {
                     HashMap<Integer, Task> allTasks = new HashMap<>();
 
                     menu.splitAndPrintTasks(taskDAO, allTasks);
-                    menu.printMainMenu();
-                    input = userInputReader.readLine();
 
-                    switch (input) {
-                        case "1":
-                            Task task = menu.addTaskPrompt();
-                            taskDAO.add(task);
-                            messageToClient.println("[+] Task added successfully!");
-                            break;
-                        case "2":
-                            try {
-                                int displayID = Integer.parseInt(menu.taskIDPrompt());
-                                int taskID = allTasks.get(displayID).getId();
-                                taskDAO.remove(taskID);
-                                messageToClient.println("[+] Task removed successfully!");
-                            } catch (NullPointerException e) {
-                                messageToClient.println("[-] Invalid task ID!");
-                            } catch (NumberFormatException e) {
-                                messageToClient.println("[-] Enter a number!");
-                            }
-                            break;
-                        case "3":
-                            try {
-                                int displayID = Integer.parseInt(menu.taskIDPrompt());
-                                int taskID = allTasks.get(displayID).getId();
-                                taskDAO.markTaskAsCompleted(taskID, LocalDateTime.now());
-                                messageToClient.println("[+] Task marked as completed!");
-                            } catch (IndexOutOfBoundsException e) {
-                                messageToClient.println("[-] Invalid task ID!");
-                            } catch (NumberFormatException e) {
-                                messageToClient.println("[-] Enter a number!");
-                            }
-                            break;
-                        case "4":
-                            try {
-                                int displayID = Integer.parseInt(menu.taskIDPrompt());
-                                Task taskToEdit = allTasks.get(displayID);
-                                Task editedTask = menu.editTaskPrompt(taskToEdit);
-                                messageToClient.println("editedTask id is: " + editedTask.getId());
-                                taskDAO.edit(editedTask.getId(),editedTask);
-                            } catch (IndexOutOfBoundsException e) {
-                                messageToClient.println("[-] Invalid task ID!");
-                            } catch (NumberFormatException e) {
-                                messageToClient.println("[-] Enter a number!");
-                            }
-                            break;
-                        case "exit":
-                            isRunning = false;
-                            messageToClient.println("{close}");
-                            socket.close();
-                            System.out.println("[+] Client disconnected > " + socket);
-                            break;
-                        default:
-                            messageToClient.println("[-] Invalid option!");
-                    }
+                    isRunning = menu.mainMenu(taskDAO, allTasks);
+
                 } catch (Exception e) {
                     messageToClient.println(e.getMessage());
                 }
             }
+            socket.close();
+            System.out.println("[+] Client disconnected > " + socket);
         } catch (IOException e) {
             System.out.println("[-] Error");
             e.printStackTrace();
