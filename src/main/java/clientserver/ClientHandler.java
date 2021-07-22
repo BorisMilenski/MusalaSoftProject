1e657da420d18f2da9a20973999eecfe0808f06f
BorisMilenski <boris.milenski@abv.bg>
2021-07-22 17:18:29 +0300

    Added missing import

--- src/main/java/clientserver/ClientHandler.java
+++ src/main/java/clientserver/ClientHandler.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 public class ClientHandler extends Thread {
